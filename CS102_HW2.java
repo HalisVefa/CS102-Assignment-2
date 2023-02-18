4d6642dc76d90807fb7307a334dcc9530aad30b9
Merve <merve.gulec@ug.bilkent.edu.tr>
2023-02-18 15:24:50 +0300

    Stage c

--- CS102_HW2.java
+++ CS102_HW2.java
@@ -45,4 +45,62 @@ public class CS102_HW2{
         }
         return minimum;
     }
-}
+
+    public static void findAvAndDifference(int[] input)
+    {
+        int sum = 0;
+        int average;
+        int[] difference = new int[input.length];
+        for(int i = 0; i < input.length; i++)
+        {
+            sum = sum + input[i];
+        }
+        average = sum / input.length;
+        
+        for(int a = 0; a < input.length; a++)
+        {
+            difference[a] = input[a] - average;
+        }
+
+        System.out.println("The average of the array is: " + average);
+        System.out.print("The differences from the average is: {");
+        for(int k = 0; k < difference.length; k++)
+        {
+            if(k != difference.length - 1)
+            {
+            System.out.print(difference[k] + ",");
+            }
+        }
+        System.out.print(difference[difference.length - 1] + " .}");
+        System.out.println();
+ 
+
+    } 
+ 
+
+    public static int sumOfOdd(int[] input)
+    {
+        int sum = 0;
+        for(int i = 1; i < input.length; i+=2)
+        {
+            sum = sum + input[i];
+        }
+        return sum;
+
+    }
+    public static int sumOfEven(int[] input)
+    {
+        int sum = 0;
+        for(int i = 0; i < input.length; i+=2)
+        {
+            sum = sum + input[i];
+        }
+        return sum;
+
+        
+    }
+
+        
+    }
+    
+
